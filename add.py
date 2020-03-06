2a6ac808fb84c6c7aa1e512a0b23005957f3e417
mguerny <marin.guerny@laposte.net>
2020-03-06 14:05:43 +0100

    BLUE - no separator at the end

--- add.py
+++ add.py
@@ -5,19 +5,22 @@ class Add:
         numbersum = 0
         if numbers == '':
             return '0'
-        list_separated_by_comma = numbers.split(',')
-        list_separated_by_newline_and_comma = []
-        for item in list_separated_by_comma:
+
+        list_by_separator = numbers.split(',')
+        list_by_separator_and_newline = []
+        for item in list_by_separator:
             smalllist = item.split(r"\n")
             for item2 in smalllist:
-                list_separated_by_newline_and_comma.append(item2)
+                list_by_separator_and_newline.append(item2)
 
-        for item in list_separated_by_newline_and_comma:
+        for item in list_by_separator_and_newline:
             if item == '':
                 return 'Number expected but EOF found'
 
-        for string in list_separated_by_newline_and_comma:
+        for string in list_by_separator_and_newline:
             if string.isnumeric():
                 numbersum += int(string)
 
         return str(numbersum)
+
+
