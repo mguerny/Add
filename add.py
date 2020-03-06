0c8e5d044a795746caee1298d1997608b92a0b12
mguerny <marin.guerny@laposte.net>
2020-03-06 13:56:07 +0100

    BLUE - newline as separator

--- add.py
+++ add.py
@@ -3,13 +3,13 @@ class Add:
     @staticmethod
     def add(numbers):
         numbersum = 0
-        liststringtemp = numbers.split(',')
-        liststring = []
-        for item in liststringtemp:
+        list_separated_by_comma = numbers.split(',')
+        list_separated_by_newline_and_comma = []
+        for item in list_separated_by_comma:
             smalllist = item.split(r"\n")
             for item2 in smalllist:
-                liststring.append(item2)
-        for string in liststring:
+                list_separated_by_newline_and_comma.append(item2)
+        for string in list_separated_by_newline_and_comma:
             if string.isnumeric():
                 numbersum += int(string)
 
