a64e5dc4dc9ab2646b388165b7ab163189714b30
mguerny <marin.guerny@laposte.net>
2020-03-06 12:54:48 +0100

    RED - 3 args sum

--- test.py
+++ test.py
@@ -7,6 +7,11 @@ class TestFizzBuzz(unittest.TestCase):
     def test_empty_should_return_0(self):
         self.assertEqual(Add.add(""), 0)
 
+    def test_should_return_sum_of_3_args(self):
+        self.assertEqual(Add.add("1"), 1)
+        self.assertEqual(Add.add("1,2"), 3)
+        self.assertEqual(Add.add("1,2,3"), 6)
+
 
 if __name__ == '__main__':
     unittest.main()
