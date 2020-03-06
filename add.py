ee8c858714aa2e135842b2be895fe844c7f1c69b
mguerny <marin.guerny@laposte.net>
2020-03-06 14:04:01 +0100

    GREEN - no separator at the end

--- add.py
+++ add.py
@@ -3,17 +3,21 @@ class Add:
     @staticmethod
     def add(numbers):
         numbersum = 0
+        if numbers == '':
+            return '0'
         list_separated_by_comma = numbers.split(',')
         list_separated_by_newline_and_comma = []
         for item in list_separated_by_comma:
             smalllist = item.split(r"\n")
             for item2 in smalllist:
                 list_separated_by_newline_and_comma.append(item2)
+
+        for item in list_separated_by_newline_and_comma:
+            if item == '':
+                return 'Number expected but EOF found'
+
         for string in list_separated_by_newline_and_comma:
             if string.isnumeric():
                 numbersum += int(string)
 
         return str(numbersum)
-
-
-
