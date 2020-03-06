f54d78f48306407bcba7496b35582caa33e09409
mguerny <marin.guerny@laposte.net>
2020-03-06 12:59:11 +0100

    BLUE - 3 args sum

--- add.py
+++ add.py
@@ -5,20 +5,10 @@ class Add:
         if numbers == '':
             return '0'
 
-        if len(numbers) == 1 and numbers.isnumeric():
-            return numbers
+        numbersum = 0
+        liststring = numbers.split(',')
+        for string in liststring:
+            if string.isnumeric():
+                numbersum += int(string)
 
-        if len(numbers) == 3:
-            s1 = numbers[0]
-            s2 = numbers[2]
-            if s1.isnumeric and s2.isnumeric():
-                return str(int(s1) + int(s2))
-
-        if len(numbers) == 5:
-            s1 = numbers[0]
-            s2 = numbers[2]
-            s3 = numbers[4]
-            if s1.isnumeric and s2.isnumeric() and s3.isnumeric():
-                return str(int(s1) + int(s2) + int(s3))
-
-
+        return numbersum
