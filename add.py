5b39dbd7893b41e562d7847f364f64e63c0ccd3b
mguerny <marin.guerny@laposte.net>
2020-03-06 13:54:46 +0100

    GREEN - newline as separator

--- add.py
+++ add.py
@@ -3,11 +3,17 @@ class Add:
     @staticmethod
     def add(numbers):
         numbersum = 0
-        liststring = numbers.split(',')
+        liststringtemp = numbers.split(',')
+        liststring = []
+        for item in liststringtemp:
+            smalllist = item.split(r"\n")
+            for item2 in smalllist:
+                liststring.append(item2)
         for string in liststring:
             if string.isnumeric():
                 numbersum += int(string)
 
         return str(numbersum)
+
 
 
